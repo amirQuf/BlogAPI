78a680164809550aa3d8fce710e902a7305ed67e
Amir ghasemian <amir.ghasemian.ag@gmail.com>
2021-04-23 14:46:32 +0430

    adding api app and writing post model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('D','draft'),
+        ('P','Publish'),
+        )
+    title = models.CharField(max_length = 200)
+    thumbnail = models.ImageField(upload_to = 'post')
+    description = models.TextField()
+    updated = models.DateTimeField(default = timezone.now)
+    user = models.ForeignKey(User,  on_delete = models.CASCADE)
+    created = models.DateTimeField(auto_now = True)
+    status = models.CharField(max_length = 1 , choices = STATUS_CHOICES)
+    def __str__(self):
+        return f"{self.title}|{self.user.username}"
+    class Meta:
+        ordering = ['-updated']
+        verbose_name = "پست"
+        verbose_name_plural = " پست ها"
+
+        
+
