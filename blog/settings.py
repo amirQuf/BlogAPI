bdbaa47b85f25f752a39a226b24027c812635fc3
Amir ghasemian <amir.ghasemian.ag@gmail.com>
2021-04-27 17:54:18 +0430

    changing settings

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static') 
+STATIC_ROOT =BASE_DIR / 'static'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media') 
+MEDIA_ROOT =  BASE_DIR  / 'Media'
 
 MEDIA_URL = '/media/'
