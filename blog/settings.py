2b6bd39101b541266f45b560586c3faeed5b212e
Amir ghasemian <amir.ghasemian.ag@gmail.com>
2021-07-18 11:27:21 +0430

    fixing bugs

--- blog/settings.py
+++ blog/settings.py
@@ -21,11 +21,11 @@ environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+DEBUG = env('DEBUG')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
