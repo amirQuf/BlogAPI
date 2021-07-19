6a2c7b384337ba45baaaf67f110ff39bb2c2e06d
Amir ghasemian <amir.ghasemian.ag@gmail.com>
2021-07-19 21:16:10 +0430

    cleaning  the models

--- web/models.py
+++ web/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-from django.shortcuts import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length = 200)
@@ -12,17 +11,23 @@ class Category(models.Model):
     parent = models.ForeignKey('self',blank=True,null=True, on_delete=models.CASCADE)
     description = models.TextField(blank=True,null=True)
     active = models.BooleanField(default=True)
+
     class Meta:
         ordering = ('-created',)
+
     def __str__(self):
         return f"{self.name}|{self.user.username}"
+
     def get_absolute_url(self):
         return f"/cat/{self.slug}"
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('D','draft'),
         ('P','Publish'),
         )
+
     title = models.CharField(max_length = 200)
     thumbnail = models.ImageField(upload_to = 'post')
     description = models.TextField()
@@ -34,8 +39,10 @@ class Post(models.Model):
     categories = models.ManyToManyField(Category,)
     likes = models.PositiveIntegerField(default = 0)
     status = models.CharField(max_length = 1 , choices = STATUS_CHOICES)
+
     def __str__(self):
         return f"{self.title}|{self.user.username}"
+
     def get_absolute_url(self):
         return f"/{self.user.username}/{self.slug}"
     
@@ -49,8 +56,10 @@ class Comment(models.Model):
     post = models.ForeignKey(Post ,related_name='comments', on_delete=models.CASCADE)
     parent = models.ForeignKey('self',blank=True,null=True, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add = True)
+
     def __str__(self):
         return f"{self.body}|{self.user.username}"
+
     class Meta:
         ordering = ('-created',)
 
@@ -59,18 +68,18 @@ class Like (models.Model):
     user = models.ForeignKey(User ,on_delete=models.CASCADE)
     post = models.ForeignKey(Post,related_name= 'like',on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now = True)
+
     def __str__(self):
        return f"{self.user.username}|{self.post.title}"
+
     class Meta:
         ordering = ('-created',)
-
 
 
 class SavePost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     saved_post = models.ForeignKey(Post,on_delete=models.CASCADE)
     time = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ('-time',)
-
-
